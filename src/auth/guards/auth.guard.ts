962ee997ff7fbcd57b71949c8f01bfdc3f80cda7
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-11-10 13:51:22 -0500

    Verificar token y usuario

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,21 +1,50 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../interfaces/jwt-payload';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor(
     private jwtService: JwtService,
-
+    private authService: AuthService,
   ){}
 
-  canActivate(
+  async canActivate(
     context: ExecutionContext,
   ): Promise<boolean> {
 
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    console.log(token);
+
+    if (!token) {
+      throw new UnauthorizedException('Hace falta el token');
+    }
+    
+    try{
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(
+        token,
+        {
+          secret: process.env.JWT_SEED
+        }
+      );
+
+      const user = this.authService.findUserById(payload.id);
+      if (!user) {
+        throw new UnauthorizedException('El usuario no existe');
+      }
+      if (!(await user).isActive){
+        throw new UnauthorizedException('El usuario no está activo');
+      }
+
+      request['user'] = user;
+      console.log(payload);
+      
+
+    } catch(error){
+      throw new UnauthorizedException();
+    }
     
 
     return Promise.resolve(true);
